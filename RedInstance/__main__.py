3dffbe834d491b8e63265fd23d933a0632488269
Ester-Mol <151984620+Ester-Mol@users.noreply.github.com>
2025-06-25 13:43:35 +0530

    Update __main__.py

--- RedInstance/__main__.py
+++ RedInstance/__main__.py
@@ -1,3 +1,6 @@
-from redbot import Red
+import os
+print("Current directory:", os.getcwd())
+print("Files in RedInstance:", os.listdir(os.path.dirname(__file__)))
 
+from redbot import Red
 Red().run()
